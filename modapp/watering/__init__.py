85cee06ebe4201c2876871681c4db2216ef623d7
kr-g <k.r.goger+github@gmail.com>
2020-06-28 12:52:24 +0200

    prepared new version

--- modapp/watering/__init__.py
+++ modapp/watering/__init__.py
@@ -9,7 +9,7 @@ https://github.com/kr-g/mpymodcore_watering/blob/master/LICENSE
 
 """
 
-VERSION = "v0.0.5"
+VERSION = "v0.0.6a"
 APPNAME = "watering"
 
 from .app import load_config, load_generators, app_config
